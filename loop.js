b84f61727b6613a89c7069b0caefd19babf191f3
khokan <khokancrmt@gmail.com>
2024-09-04 16:44:17 +0600

    complet the practice

---
+++ loop.js
@@ -0,0 +1,17 @@
+const mobile = {
+    brand: 'vivo',
+    price:25000,
+    color: 'black',
+    camera: '12mp'
+}
+for(const prop in mobile) {
+//     console.log(prop);
+//     console.log(mobile[prop]);
+}
+const keys = Object.keys(mobile);
+console.log(keys);
+
+for(const key of keys){
+    // console.log(key);
+    console.log(key, ':' ,mobile[key]);
+}
---
+++ objectDeclar.js
@@ -0,0 +1,5 @@
+const pen = {brand: 'matador', price: 10, color: 'blue'}
+const pencil = new Object();
+console.log(pencil);
+const robber = Object.create({});
+console.log(robber);
